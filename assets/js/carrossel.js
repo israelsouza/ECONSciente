2d0d62f52f9b1400605d709320c0103fdb93d273
Israel de Souza <israellimas@hotmail.com>
2024-10-26 22:07:12 -0300

    [feat] adição itens carrossel
    [feat] - adição de novos itens no carrossel; - alteração na lógica da validação para comportar novos itens; - adição de funções para ocultar e exibir itens do carrossel; - preparação para novos benefícios que o projeto oferece

--- assets/js/carrossel.js
+++ assets/js/carrossel.js
@@ -1,86 +1,117 @@
 var posicao = 1;
-        
-        // 01 fruta
-        var f01 = document.getElementById('f01')
-        var f02 = document.getElementById('f02')
-        var f03 = document.getElementById('f03')
-        
-        // 02 legume
-        var l01 = document.getElementById('l01')
-        var l02 = document.getElementById('l02')
-        var l03 = document.getElementById('l03')
-        
-        // 03 verdura
-        var v01 = document.getElementById('v01')
-        var v02 = document.getElementById('v02')
-        var v03 = document.getElementById('v03')
-        
-        function proximaExibicao(){
 
-            if (posicao == 1) {
-                f01.style.display = "none";
-                f02.style.display = "none";
-                f03.style.display = "none";
-                
-                l01.style.display = 'block';
-                l02.style.display = 'block';
-                l03.style.display = 'block';
+/* vai capturar todas div que tem a estrutura 'item-carrossel-' e montar uma lista com esses elementos, podendo dps tratar de forma individual */
+var lista = document.querySelectorAll('div[id^="item-carrossel-"]');
 
-                posicao = posicao + 1
-            } else if (posicao == 2) {
-                l01.style.display = 'none';
-                l02.style.display = 'none';
-                l03.style.display = 'none';
-
-                v01.style.display = 'block';
-                v02.style.display = 'block';
-                v03.style.display = 'block';
-
-                posicao = posicao + 1
-            } else if (posicao == 3) {
-                v01.style.display = 'none';
-                v02.style.display = 'none';
-                v03.style.display = 'none';
-
-                f01.style.display = 'block';
-                f02.style.display = 'block';
-                f03.style.display = 'block';
-
-                posicao = 1
-            }
-        }
+function darDisplayNone(X, Y) {
+    for (let i = X; i <= Y; i++) {
+        lista[i].style.display = 'none'
+    }
+}
 
 
-        function anteriorExibicao() {
-            if (posicao == 1) {
-                f01.style.display = "none";
-                f02.style.display = "none";
-                f03.style.display = "none";
-                
-                v01.style.display = 'block';
-                v02.style.display = 'block';
-                v03.style.display = 'block';
+function darDisplayBlock(X, Y) {
+    for (let i = X; i <= Y; i++) {
+        lista[i].style.display = 'block'
+    }
+}
 
-                posicao = 3
-            } else if (posicao == 2) {
-                l01.style.display = 'none';
-                l02.style.display = 'none';
-                l03.style.display = 'none';
+function proximaExibicao() {
 
-                f01.style.display = 'block';
-                f02.style.display = 'block';
-                f03.style.display = 'block';
+    if (posicao == 1) {
+        darDisplayNone(0, 2)
+        darDisplayBlock(3, 5)
 
-                posicao = 1
-            } else if (posicao == 3) {
-                v01.style.display = 'none';
-                v02.style.display = 'none';
-                v03.style.display = 'none';
+        posicao = posicao + 1
+    } else if (posicao == 2) {
+        darDisplayNone(3, 5)
+        darDisplayBlock(6, 8)
 
-                l01.style.display = 'block';
-                l02.style.display = 'block';
-                l03.style.display = 'block';
+        posicao = posicao + 1
+    } else if (posicao == 3) {
+        darDisplayNone(6, 8)
+        darDisplayBlock(9, 11)
 
-                posicao = 2
-            }
-        }
+        posicao = posicao + 1
+    } else if (posicao == 4) {
+        darDisplayNone(9, 11)
+        darDisplayBlock(12, 14)
+
+        posicao = posicao + 1
+    } else if (posicao == 5) {
+        darDisplayNone(12, 14)
+        darDisplayBlock(15, 17)
+
+        posicao = posicao + 1
+    } else if (posicao == 6) {
+        darDisplayNone(15, 17)
+        darDisplayBlock(18, 20)
+
+        posicao = posicao + 1
+    } else if (posicao == 7) {
+        darDisplayNone(18, 20)
+        darDisplayBlock(21, 23)
+
+        posicao = posicao + 1
+    } else if (posicao == 8) {
+        darDisplayNone(21, 23)
+        darDisplayBlock(24, 26)
+
+        posicao = 0
+    } else if (posicao == 0) {
+        darDisplayNone(24, 26)
+        darDisplayBlock(0, 2)
+
+        posicao = posicao + 1
+    }
+}
+
+
+function anteriorExibicao() {
+    if (posicao == 1) {
+        darDisplayNone(0, 2)
+        darDisplayBlock(24, 26)
+
+        posicao = posicao - 1
+    } else if (posicao == 2) {
+        darDisplayNone(3, 5)
+        darDisplayBlock(0, 2)
+
+        posicao = posicao - 1
+    } else if (posicao == 3) {
+        darDisplayNone(6, 8)
+        darDisplayBlock(3, 5)
+
+        posicao = posicao - 1
+    } else if (posicao == 4) {
+        darDisplayNone(9, 11)
+        darDisplayBlock(6, 8)
+
+        posicao = posicao - 1
+    } else if (posicao == 5) {
+        darDisplayNone(12, 14)
+        darDisplayBlock(9, 11)
+
+        posicao = posicao - 1
+    } else if (posicao == 6) {
+        darDisplayNone(15, 17)
+        darDisplayBlock(12, 14)
+
+        posicao = posicao - 1
+    } else if (posicao == 7) {
+        darDisplayNone(18, 20)
+        darDisplayBlock(15, 17)
+
+        posicao = posicao - 1
+    } else if (posicao == 8) {
+        darDisplayNone(21, 23)
+        darDisplayBlock(18, 20)
+
+        posicao = posicao - 1
+    } else if (posicao == 0) {
+        darDisplayNone(24, 26)
+        darDisplayBlock(21, 23)
+
+        posicao = 8
+    }
+}
