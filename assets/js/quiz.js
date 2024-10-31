482b1a803a962862e3c972e06a8f078335cefb0f
Israel de Souza <israellimas@hotmail.com>
2024-10-31 01:02:23 -0300

    [refactor/feat] simplificação
    (refactor) simplificação na captura de questões e alternativas usando querySelector;

--- assets/js/quiz.js
+++ assets/js/quiz.js
@@ -1,4 +1,54 @@
-var questao01 = document.getElementById('questao01');
+// captura todos os botoes e insere em um Array
+const btn = document.querySelectorAll('button');
+
+// captura todas as divs que possuem o trecho 'questao' em comum e coloca em um Array
+const question = document.querySelectorAll('div[id^="questao"]');
+
+
+var pontos = 0;
+var contador = 0;
+var limiteContador = question.length;
+
+
+// ADD EVENT LISTNER
+// se o botao[0], ocorre as verificações (soma se correto, e troca questão) 
+// se o botão[1], retorna uma questão
+//      se a questão anterior foi certa, precisa diminuir um ponto ao retornar
+
+
+function proximaQuestao() {
+
+    const inputCheck = document.getElementsByTagName('input')
+  
+    for (var i = 0; i < inputCheck.length; i++) {
+        if(inputCheck[i].value == 1 && inputCheck[i].checked) {
+            console.log(inputCheck[i].value);
+            somar();
+            alterandoQuestao();
+        } else if (inputCheck[i].checked) {
+            console.log(inputCheck[i].value);
+            alterandoQuestao();
+        }
+    }
+}
+
+function alterandoQuestao(){
+    if ( contador != (limiteContador - 1) ) {
+        question[contador].style.display = 'none';
+        contador = contador + 1;
+        question[contador].style.display = 'block';
+    } else {
+        question[contador].style.display = 'none';
+        // exibir a parte de resultado com a soma dos pontos
+    }
+}
+
+function somar() {
+    pontos = pontos + 1;
+}
+
+
+/* var questao01 = document.getElementById('questao01');
 var questao02 = document.getElementById('questao02');
 var questao03 = document.getElementById('questao03');
 var questao04 = document.getElementById('questao04');
@@ -11,34 +61,4 @@ var questao10 = document.getElementById('questao10');
 
 todasQuestoes = [
     questao01, questao02, questao03, questao04, questao05, questao06, questao07, questao08, questao09, questao10
-];
-
-var pontos = 0;
-var contador = 0;
-var limiteContador = todasQuestoes.length;
-
-function selecionado(val) {
-    soma(val);    
-    alterandoQuestao()
-}
-
-function alterandoQuestao(){
-    if ( contador != (limiteContador - 1) ) {
-        todasQuestoes[contador].style.display = 'none';
-        contador = contador + 1;
-        todasQuestoes[contador].style.display = 'block';
-    } else {
-        todasQuestoes[contador].style.display = 'none';
-        // exibir a parte de resultado com a soma dos pontos
-    }
-}
-
-function soma(val) {
-    if (val == 1) {
-        pontos = pontos + 1;
-        console.log('Você acertou essa questão, receba um ponto.')
-        console.log('Pontos atuais: ' + pontos);
-    } else {
-        console.log('você não acertou, que pena. quem sabe a próxima você consiga');
-    }
-}
+]; */
