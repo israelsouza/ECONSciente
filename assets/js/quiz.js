442f401344983a94752f4758499a579419aa19fa
Israel de Souza <israellimas@hotmail.com>
2024-11-04 11:43:59 -0300

    [refactor/feat]
    - (feat) exibir a tela de resultado
    - (refactor) mudança de nome para ficar mais claro
    - (refactor) exclusão comentários e organização por grupo (declarações, funções)

--- assets/js/quiz.js
+++ assets/js/quiz.js
@@ -1,24 +1,27 @@
-// captura todos os botoes e insere em um Array
-const btn = document.querySelectorAll('button');
+/* COMETÁRIOS IMPORTANTES */
 
-// captura todas as divs que possuem o trecho 'questao' em comum e coloca em um Array
-const question = document.querySelectorAll('div[id^="questao"]');
+// todos inputs DO BLOCO para validações
+// os 4 campos do bloco atual estão vazios
+// o campo clicado é o correto (SOMAR)
 
+
+
+/* TODAS VARIAVEIS  E  CAPTURAS */
+
+
+// captura todas divs que possuem o trecho em comum e coloca em um Array
+const question = document.querySelectorAll('div[id^="blocoQuestao"]');
+const telaDeResultado = document.getElementById('resultado')
+const inputCheck = document.getElementsByTagName('input')
 
 var pontos = 0;
 var contador = 0;
 var limiteContador = question.length;
 
 
-// ADD EVENT LISTNER
-// se o botao[0], ocorre as verificações (soma se correto, e troca questão) 
-// se o botão[1], retorna uma questão
-//      se a questão anterior foi certa, precisa diminuir um ponto ao retornar
-
-
+/* TODAS AS FUNÇOES */
 function proximaQuestao() {
 
-    const inputCheck = document.getElementsByTagName('input')
   
     for (var i = 0; i < inputCheck.length; i++) {
         if(inputCheck[i].value == 1 && inputCheck[i].checked) {
@@ -39,26 +42,10 @@ function alterandoQuestao(){
         question[contador].style.display = 'block';
     } else {
         question[contador].style.display = 'none';
-        // exibir a parte de resultado com a soma dos pontos
+        telaDeResultado.style.display = 'block'
     }
 }
 
 function somar() {
     pontos = pontos + 1;
 }
-
-
-/* var questao01 = document.getElementById('questao01');
-var questao02 = document.getElementById('questao02');
-var questao03 = document.getElementById('questao03');
-var questao04 = document.getElementById('questao04');
-var questao05 = document.getElementById('questao05');
-var questao06 = document.getElementById('questao06');
-var questao07 = document.getElementById('questao07');
-var questao08 = document.getElementById('questao08');
-var questao09 = document.getElementById('questao09');
-var questao10 = document.getElementById('questao10');
-
-todasQuestoes = [
-    questao01, questao02, questao03, questao04, questao05, questao06, questao07, questao08, questao09, questao10
-]; */
