0cbc8b0e5559c7e85291231a921f283f9d9ff3ed
Israel de Souza <israellimas@hotmail.com>
2024-11-10 18:57:59 -0300

    [refactor]

--- assets/js/quiz.js
+++ assets/js/quiz.js
@@ -11,26 +11,52 @@
 
 // captura todas divs que possuem o trecho em comum e coloca em um Array
 const question = document.querySelectorAll('div[id^="blocoQuestao"]');
+const alternativa = document.getElementsByTagName('input')
 const telaDeResultado = document.getElementById('resultado')
-const inputCheck = document.getElementsByTagName('input')
 
 var pontos = 0;
-var contador = 0;
+var contador = 0;                       // bloco de questão atual.
 var limiteContador = question.length;
 
 
 /* TODAS AS FUNÇOES */
 function proximaQuestao() {
+    // primeiro validar se os imputs do bloco estão todos vazios
+    // se estiverem, dar ERRO, senão segue o fluxo
 
-  
-    for (var i = 0; i < inputCheck.length; i++) {
-        if(inputCheck[i].value == 1 && inputCheck[i].checked) {
-            console.log(inputCheck[i].value);
+    validarAlternativaEmBranco();
+    console.log('eu entro aqui?')
+
+    for (var i = 0; i < alternativa.length; i++) {
+        if(alternativa[i].value == 1 && alternativa[i].checked) {
+            console.log(alternativa[i].value);
             somar();
             alterandoQuestao();
-        } else if (inputCheck[i].checked) {
-            console.log(inputCheck[i].value);
-            alterandoQuestao();
+        }
+    }
+
+}
+
+var valorCasaInicial = 0; // 4  8   12  16
+var valorcasFinal = 3;  // 7    11  15  19
+
+function validarAlternativaEmBranco(){
+    // ter o bloco atual e alternativas desse bloco
+
+    if (contador == 0) {
+        for (let i = valorCasaInicial; i < valorcasFinal; i++) {
+        }
+
+        alt01 = alternativa[valorCasaInicial]
+        alt02 = alternativa[valorCasaInicial+1]
+        alt03 = alternativa[valorCasaInicial+2]
+        alt04 = alternativa[valorCasaInicial+3]
+
+        if (alt01.checked == false && alt02.checked == false && alt03.checked == false && alt04.checked == false ) {
+            alert('selecione alguma alternativa')
+            return break;
+        } else{
+            valorCasaInicial = valorCasaInicial + 4;
         }
     }
 }
