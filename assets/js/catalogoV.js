75c814e04121f7aeff0655ff1a89e7b20804bd83
Israel de Souza <israellimas@hotmail.com>
2024-11-15 18:52:03 -0300

    [feat]
    - página nova
    - imagens atribuídas
    - navegação entre as pesquisas

---
+++ assets/js/catalogoV.js
@@ -0,0 +1,28 @@
+const caminhoVERDURA = "./assets/img/verdura/";
+const blocos = document.querySelectorAll("div.col");
+
+const verdura = [
+    caminhoVERDURA + "acelga_isolated.jpg",
+    caminhoVERDURA + 'agriao_isolated.png',
+    caminhoVERDURA + 'alface_isolated.png',
+    caminhoVERDURA + 'brocolis_isolated.jpg',
+    caminhoVERDURA + 'cebolinha_isolated.png',
+    caminhoVERDURA + 'coentro_isolated.png',
+    caminhoVERDURA + 'couve_isolated.png',
+    caminhoVERDURA + 'couveflor_isolated.png',
+    caminhoVERDURA + 'espinafre_isolated.png',
+    caminhoVERDURA + 'manjericao_isolated.png',
+    caminhoVERDURA + '',
+    caminhoVERDURA + '',
+    caminhoVERDURA + 'repolho_isolated.png',
+    caminhoVERDURA + 'rucula_isolated.jpg',
+    caminhoVERDURA + 'salsao_isolated.png',
+    caminhoVERDURA + 'salsinha_isolated.png',
+];
+
+for (let i = 0; i < verdura.length; i++) {
+    blocos[i].style.backgroundImage = `url( ${verdura[i]} )`;
+    blocos[i].style.backgroundPosition = 'center';
+    blocos[i].style.backgroundSize = 'contain';
+    blocos[i].style.backgroundRepeat = 'no-repeat';
+}
