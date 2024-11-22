ff9025a8346f590bc26f1f098614a2e1ec6830b6
Israel de Souza <israellimas@hotmail.com>
2024-11-22 02:05:11 -0300

    [refactor] refatoração da função

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,28 +1,3 @@
-/* M E N U   C A B E Ç A L H O */
-
-const menu = document.getElementById('menu-hamb')
-const barraLateral = document.getElementById('barraLateral')
-
-var barraAtiva = false
-
-function mostrarBarraLateral() {
-    // ao clicar em menu, a barra apareça, ao clicar no menu novamente, a barra desaparece
-    if (barraAtiva == false) {
-        barraLateral.style.display = 'block'
-        barraLateral.style.textAlign = 'rigth'
-
-        barraAtiva = true
-    } else if (barraAtiva == true) {
-        //esconde a barra
-        barraLateral.style.display = 'none'
-
-
-        barraAtiva = false
-    }
-}
-
-
-
 /* M A I N - I N T R O */
 
 const introMissao = document.getElementById('intro-missao')
