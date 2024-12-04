b74ab9db37dea6953622654e558120dd0d357d84
Israel de Souza <israellimas@hotmail.com>
2024-12-04 11:58:03 -0300

    [chore] adição medalha

--- assets/js/quiz.js
+++ assets/js/quiz.js
@@ -47,11 +47,11 @@ function exibirMensaguemComPontuacao(nameUser) {
 
 
     if (pontosUsuario < 5) {
-        return mensagem = 'Não desanime, ' + nameUser + '. Continue praticando e você melhorará! <p><iframe src="https://giphy.com/embed/3og0IxUJxtCFRJfXcQ" width="480" height="269" style="" frameBorder="0" class="giphy-embed" allowFullScreen></iframe></p>';
+        return mensagem = 'Não desanime, ' + nameUser + '. Continue praticando e você melhorará! 🥉🥉 <p><iframe src="https://giphy.com/embed/3og0IxUJxtCFRJfXcQ" width="480" height="269" style="" frameBorder="0" class="giphy-embed" allowFullScreen></iframe></p>';
     } else if (pontosUsuario < 7) {
-        return mensagem = 'Bom trabalho, ' + nameUser + '! Você teve um bom desempenho. <p><iframe src="https://giphy.com/embed/tIeCLkB8geYtW" width="480" height="379" style="" frameBorder="0" class="giphy-embed" allowFullScreen></iframe></p>';
+        return mensagem = 'Bom trabalho, ' + nameUser + '! Você teve um bom desempenho. 🥈🥈 <p><iframe src="https://giphy.com/embed/tIeCLkB8geYtW" width="480" height="379" style="" frameBorder="0" class="giphy-embed" allowFullScreen></iframe></p>';
     } else if (pontosUsuario < 11) {
-        return mensagem = 'Parabéns, ' + nameUser + '! Você teve um desempenho excelente! <p><iframe src="https://giphy.com/embed/11ISwbgCxEzMyY" width="480" height="360" style="" frameBorder="0" class="giphy-embed" allowFullScreen></iframe></p>';
+        return mensagem = 'Parabéns, ' + nameUser + '! Você teve um desempenho excelente! 🥇🥇 <p><iframe src="https://giphy.com/embed/11ISwbgCxEzMyY" width="480" height="360" style="" frameBorder="0" class="giphy-embed" allowFullScreen></iframe></p>';
     } else {
         return mensagem = 'Erro ao processar a sua pontuação, tente novamente, por favor.';
     }
